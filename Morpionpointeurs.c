4b2feacd94e04a4838342a248d7d5c3ca05ca554
zozio28 <zoe.artigas@gmail.com>
2023-04-13 14:19:34 +0200

    finally

--- Morpionpointeurs.c
+++ Morpionpointeurs.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //printf,scanf
 #include <stdlib.h>
+#define TAILLE 3 //taille du tableau
 
 //Fonction qui permet d'allouer un tablau dynamique
 int** allouerTableau(int nb_lignes,int nb_colonnes){
@@ -22,10 +23,10 @@ void initTableau(int** maGrille,int nb_lignes,int nb_colonnes){
 
 //Procédure qui permet d'afficher un tableau dynamique
 void afficherGrille(int** maGrille){
-    for (int i=0; i<3; i++){
+    for (int i=0; i<TAILLE; i++){
         printf("+---+---+---+ \n");
         printf("|");
-        for (int j=0; j<3; j++){
+        for (int j=0; j<TAILLE; j++){
             switch (maGrille[i][j]){
                 case 1:
                     printf(" X |");
@@ -44,12 +45,26 @@ void afficherGrille(int** maGrille){
     printf("\n");
 }
 
+//Procédure qui permet à un joueur de placer son pion
 void joue(int** maGrille, int numeroJoueur,int i,int j){
-    maGrille[i][j]=numeroJoueur;
+    if (maGrille[i][j] == 0) {
+        maGrille[i][j] = numeroJoueur;
+    }
+    else {
+        printf("Case déjà occupée !\n");
+    }
 }
 
-void tour2jeu(int** maGrille,int* joueur){
-
+//Procédure qui permet à un joueur de saisir les coordonnées de son pion
+void tour2jeu(int** maGrille, int* joueur, int* i, int* j) {
+    //demande au joueur de jouer tant que ses coordonnées sont inexactes
+    do {
+        printf("Joueur %d, veuillez entrer la ligne (0-2) : ", *joueur);
+        scanf("%d", i);
+        printf("Veuillez entrer la colonne (0-2) : ");
+        scanf("%d", j);
+    } while ((*i > 2) || (*i < 0) || (*j > 2) || (*j < 0) || (maGrille[*i][*j] != 0));
+    //change de joueur
     switch(*joueur){
         case 1 :
             *joueur = 2;
@@ -61,27 +76,65 @@ void tour2jeu(int** maGrille,int* joueur){
     
 }
 
+//Prédicat qui permet de tester si un joueur a gagné
+int gagne (int** maGrille, int joueur){
+    // Vérification des lignes
+    for (int i = 0; i < TAILLE; i++) {
+        if (maGrille[i][0] == joueur && maGrille[i][1] == joueur && maGrille[i][2] == joueur) {
+            return 1;
+        }
+    }
+    // Vérification des colonnes
+    for (int i = 0; i < TAILLE; i++) {
+        if (maGrille[0][i] == joueur && maGrille[1][i] == joueur && maGrille[2][i] == joueur) {
+            return 1;
+        }
+    }
+    // Vérification de la diagonale haut-gauche vers bas-droite
+    if (maGrille[0][0] == joueur && maGrille[1][1] == joueur && maGrille[2][2] == joueur) {
+        return 1;
+    }
+    // Vérification de la diagonale bas-gauche vers haut-droite
+    if (maGrille[2][0] == joueur && maGrille[1][1] == joueur && maGrille[0][2] == joueur) {
+        return 1;
+    }
+    return 0;
+}
+
+//Fonction principake
 int main(){
+    
+    //Variables
     int** tab = NULL;
     int lignes = 3;
     int colonnes = 3;
     int joueur = 1;
+    int j = 0;
+    int nbCoups = 0;
 
+    //alloue l'espace pour tab et initialise le tableau
     tab = allouerTableau(lignes,colonnes);
     initTableau(tab,lignes,colonnes);
-    afficherGrille(tab);
-    for (int i = 0; i<lignes*colonnes; i++){
-        do {
-            printf("Veuillez entrez la ligne suivie de la ligne : ");
-            scanf("%d", &i);
-            printf("Veuillez entrer la colonne : ");
-            scanf("%d", &j);
-        } while (((i > 2) || (i < 0)) || ((j > 2) || (j < 0)) || (maGrille[i][j]!=0));
-        joue(tab,joueur,lignes,colonnes);
-        afficherGrille(tab);
-        tour2jeu(tab,joueur,lignes,colonnes);
-    }
 
+    //boucle de jeu
+    for (int i = 0; i<lignes*colonnes; i++){
+        //affiche la grille joue le coup donné par le joueur
+        afficherGrille(tab);
+        tour2jeu(tab,&joueur,&i,&j);
+        joue(tab,joueur,i,j);
+        nbCoups++;// incrémente le compteur de coups
+        if (gagne(tab,joueur)) { // vérifie si un joueur a gagné
+            afficherGrille(tab); 
+            printf("Joueur %d a gagné !\n", joueur);
+            break;
+        } else if (nbCoups == lignes*colonnes) { // vérifie si la grille est remplie
+            afficherGrille(tab);
+            printf("Match nul !\n");
+            break;
+        }
+    }
+    
+    //libère l'espace alloué
     free(tab);
     tab = NULL;
 
